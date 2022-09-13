71e14c067134dcd26c21488d43437af3e25a76e1
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-09-12 23:16:45 -0400

    incorporated joke API

--- script.js
+++ script.js
@@ -104,17 +104,36 @@ const VoiceRSS = {
   },
 };
 
-function test() {
-  VoiceRSS.speech({
-    key: "07f002ee890346959306ca9ee461e8de",
-    src: "Hello, world!",
-    hl: "en-us",
-    v: "Linda",
-    r: 0,
-    c: "mp3",
-    f: "44khz_16bit_stereo",
-    ssml: false,
-  });
-}
+// function test() {
+//   VoiceRSS.speech({
+//     key: "07f002ee890346959306ca9ee461e8de",
+//     src: "Hello, world!",
+//     hl: "en-us",
+//     v: "Linda",
+//     r: 0,
+//     c: "mp3",
+//     f: "44khz_16bit_stereo",
+//     ssml: false,
+//   });
+// }
 
-test();
+// test();
+
+//Get jokes from Joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    console.log(joke);
+  } catch (error) {
+    console.log("whoops", error);
+  }
+}
